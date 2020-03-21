2607870fb10fa05c1f9caf7bff71c9cf97c27b0f
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-21 16:00:14 -0400

    progress changes

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -2,11 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import model burger.js
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 
 //Get route for burger data
 router.get("/", function (req, res) {
-    burger.all(function (data) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -15,26 +15,26 @@ router.get("/", function (req, res) {
 });
 // Burger is posted to page
 router.post("/api/burgers", function (req, res) {
-    burger.create(
-        req.body.name,
-     function (result) {
-        res.json({ id: result.insertId });
-    });
+    burger.insertOne(["burger_name", "devoured"],
+        [req.body.burger_name, req.body.devoured], function (result) {
+            res.json({ id: result.insertId });
+        });
 });
+
 //Burger is updated from other side of page
 router.put("/api/burgers/:id", function (req, res) {
     var id = req.params.id;
     var devoured = req.body.devoured;
 
-    burger.update(
-        devoured,
-        id, function (result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, id, function (result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
 });
 
 module.exports = router;
