064d856acb139bacd9a4643e7a3a241b968095e3
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-21 14:47:29 -0400

    added two columns in handlebars, added new image

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
+// Content from public directory
 app.use(express.static("public"));
 
 // Parse application body as JSON
@@ -14,13 +15,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them
+// Import routes & give server access
 var routes = require("./controllers/burger-controller.js");
 
 app.use(routes);
 
-// Start the server to listen to client requests
+// Start server to listen to client requests
 app.listen(PORT, function () {
-  // Log (server-side) when the server starts
+  // Log server-side
   console.log("Server listening on: http://localhost:" + PORT);
 });
